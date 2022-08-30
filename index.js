234be6991c124f4dd42b3ffc8041c818c982d35d
Francis Maina <sebastianfrancix@gmail.com>
2022-08-31 00:50:03 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
+const { stringify } = require("mocha/lib/utils");
+
 function isPalindrome(word) {
-  // Write your algorithm here
+  //splitting the word, reversing it then joining it back together
+  const palindromeWord = word.split("");
+  const reversedPalindrome = palindromeWord.reverse();
+  const reversedString = reversedPalindrome.join("");
+
+  //Checking if the reversedString is the same as the word passed into the function
+
+  if (word === reversedString) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 /* 
   Add your pseudocode here
-*/
+// checking if the word is a string
+// split, reverse, then join the string back to form a word
+// check if the reversed string is the same as the initial word
 
 /*
   Add written explanation of your solution here
